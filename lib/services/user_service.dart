cf8ca7a8c3a4e8d7b0f40d1087477d913b73aa6e
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-09-26 19:02:41 -0500

    Fix to userservice late initiation

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tests_flutter/model/user.dart';
 
 class UserService with ChangeNotifier {
-  late User _user;
-  User get user => _user;
+  User? _user;
+  User get user => _user!;
   bool get userExist => _user != null ? true : false;
 }
